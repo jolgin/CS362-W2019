b1df22d0361e27712fda69ffa286ae6a45645239
jolgin <jolgin90@gmail.com>
2019-03-02 14:14:44 -0600

    assignment 4 updates

---
+++ projects/olginj/dominion/randomtestcard1.c
@@ -0,0 +1,8 @@
+//
+//  randomtestcard1.c
+//  
+//
+//  Created by John Olgin on 2/27/19.
+//
+
+#include <stdio.h>
---
+++ projects/olginj/dominion/randomtestcard2.c
@@ -0,0 +1,8 @@
+//
+//  randomtestcard2.c
+//  
+//
+//  Created by John Olgin on 2/27/19.
+//
+
+#include <stdio.h>
